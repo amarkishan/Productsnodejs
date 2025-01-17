6265c807a285c10d5135f356ff4be3a9f9431e54
amarkishan <amar6333kartheek@gmail.com>
2025-01-17 11:55:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ app.get('/', (req, res) => {
 
 
 
-// Corrected MongoDB URI and mongoose connection
-mongoose
-    .connect("mongodb://127.0.0.1:27017/ecom1", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Connected to MongoDB");
-        // app.listen(9090, () => {
-        //     console.log("Server is running on http://localhost:8080");
-        // });
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-    });
+// // Corrected MongoDB URI and mongoose connection
+// mongoose
+//     .connect("mongodb://127.0.0.1:27017/ecom1", { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+//         // app.listen(9090, () => {
+//         //     console.log("Server is running on http://localhost:8080");
+//         // });
+//     })
+//     .catch((error) => {
+//         console.error("Error connecting to MongoDB:", error);
+//     });
 
 app.listen(9090, () => {
             console.log("Server is running on http://localhost:8080");
